429e4339f39f2da6711f6a3380da8aae96571857
leafartech <119262053+leafartech@users.noreply.github.com>
2023-04-24 18:12:26 -0300

    Delete src directory

--- src/pages/Metabolic/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './Metabolic.module.css'
-
-export default function Metabolic() {
-    return (
-        <div className={ styles.metabolic }>
-            Metabolic main page
-        </div>
-    )
-}
