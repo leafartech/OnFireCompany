627c3bad99c6581cda9b3be3577e60a07a166f46
leafartech <119262053+leafartech@users.noreply.github.com>
2023-05-11 10:01:55 -0300

    Delete src directory

--- src/components/Img/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './Img.module.css'
-
-export default function Img({ path, alt }) {
-    return (
-        <div className={ styles.img }>
-            <img src={ path } alt={alt} />
-        </div>
-    )
-}
